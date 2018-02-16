435a59e8e673ab4c50b3f0e224fc616be0526813
didi <didi@192.168.31.231>
2018-02-16 22:31:05 +0800

    0216 22:30

---
+++ api-gateway/src/main/java/com/autobot/api/gateway/Application.java
@@ -0,0 +1,17 @@
+package com.autobot.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
