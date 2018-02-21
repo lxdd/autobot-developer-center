7000dff83ba7c598cf040796fb826b7a1bcf3ef2
didi <didi@192.168.31.231>
2018-02-21 14:02:50 +0800

    spring cloud config

--- config-server/src/main/java/com/autobot/config/server/Application.java
+++ config-server/src/main/java/com/autobot/config/server/Application.java
@@ -1,18 +1,20 @@
 package com.autobot.config.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- * Hello world!
+ * config-server 通过Git 在本地仓库暂存，可以有效防止当 Git 仓库出现故障而引起 无法加载配置信息的情况。
+ * 
  *
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class Application 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
