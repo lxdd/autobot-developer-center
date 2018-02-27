fa842851a58c54874dafadbab1d29c900c93f777
didi <didi@211.136.113.1>
2018-02-27 22:52:31 +0800

    0227 22：52

--- autobot-res/src/main/java/com/autobot/res/adc/service/CatalogService.java
+++ autobot-res/src/main/java/com/autobot/res/adc/service/CatalogService.java
@@ -1,5 +1,13 @@
 package com.autobot.res.adc.service;
 
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 目录服务层
+ * @author li_xiaodong
+ * @date: 2018年2月27日 下午10:49:58 
+ */
+@Service("catalogService")
 public class CatalogService {
 
 }
